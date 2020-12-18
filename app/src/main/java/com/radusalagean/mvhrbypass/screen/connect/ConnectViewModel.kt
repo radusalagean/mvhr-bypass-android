8948a08725faffcadc74b34b1c77ccaf831b3fbc
Radu Salagean <radusalagean@hotmail.com>
2020-12-18 16:50:52 +0200

    Display version name on Connect Screen

--- app/src/main/java/com/radusalagean/mvhrbypass/screen/connect/ConnectViewModel.kt
+++ app/src/main/java/com/radusalagean/mvhrbypass/screen/connect/ConnectViewModel.kt
@@ -1,6 +1,7 @@
 package com.radusalagean.mvhrbypass.screen.connect
 
 import androidx.lifecycle.MutableLiveData
+import com.radusalagean.mvhrbypass.BuildConfig
 import com.radusalagean.mvhrbypass.generic.activity.ActivityContract
 import com.radusalagean.mvhrbypass.generic.viewmodel.BaseViewModel
 import com.radusalagean.mvhrbypass.persistence.sharedprefs.SharedPreferencesRepository
@@ -8,6 +9,8 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class ConnectViewModel : BaseViewModel(), KoinComponent {
+
+    val versionName: String = BuildConfig.VERSION_NAME
 
     private val sharedPreferencesRepository by inject<SharedPreferencesRepository>()
     val address = MutableLiveData<String>()
